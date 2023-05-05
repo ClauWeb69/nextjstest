49afe0a43d855edef3ab8c2b78a2ef947d8c270b
ClauWeb <claudiese00@gmail.com>
2023-05-05 11:31:51 +0200

    first commit

---
+++ src/app/dashboard/pages/page.js
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Sidebar() {
+
+    return (
+        
+        <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3">
+            
+        </div>
+        
+    );
+}
---
+++ src/app/home/page.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h1>Benvenuto nella homepage!</h1>
+        <p>Questa è una pagina di esempio creata con Next.js.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
